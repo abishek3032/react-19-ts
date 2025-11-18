95f3b028edd858a031f32375849bd5979dee1a1d
abishek3032 <abishekyo22@gmail.com>
2025-11-18 22:45:09 +0530

    made modren ui

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -11,16 +11,6 @@ const Products = (props: any) => {
                     </div>
                 )
             )}
-
-            {props.products.map((product: ProductType) =>
-                product.isActive ? (
-                    <div className="flex gap-2 border-2 m-3 p-3 rounded " key={product.id}>
-                        <div>{product.id}</div>
-                        <div>{product.name}</div>
-                        <div>{product.price}</div>
-                    </div>
-                ) : (<p>Not Active</p>)
-            )}
         </>
     );
 }
